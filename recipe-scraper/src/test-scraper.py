bf8e8dcd0830b8464446ef6c2fca2fc751b963b6
Lu Wang <luwang.wang@gmail.com>
2024-08-14 14:34:09 -0400

    housekeeping: deleted old test files

--- recipe-scraper/src/test-scraper.py
+++
@@ -1,7 +0,0 @@
-from lib.recipes import get_nutrition_from_url
-
-
-url = 'https://cafedelites.com/authentic-chimichurri-uruguay-argentina/'
-servings = 1.0
-result = get_nutrition_from_url(url=url, servings=servings)
-print(result)
