32a61888e81ffec9f4b906798e4fa6eb0fbdfd4e
Lu Wang <luwang.wang@gmail.com>
2024-08-18 16:25:15 -0400

    fix: spacing change on calculator

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,7 @@ import InputForm from "./ui/Calculator/InputForm";
 export default async function Home() {
   const pong = await pingBackend()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start py-24 px-10">
+    <main className="flex min-h-screen flex-col items-center justify-start py-0 px-10">
       <InputForm />
     </main>
   );
