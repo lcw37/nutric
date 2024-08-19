819c3f5edb3dc2eb7988af495fe6230016bb5d64
Lu Wang <luwang.wang@gmail.com>
2024-08-19 11:01:12 -0400

    ui tweak

--- frontend/app/(main)/handler/[...stack]/layout.tsx
+++ frontend/app/(main)/handler/[...stack]/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({
     children: React.ReactNode;
 }>) {
     return (
-        <div className="px-8 md:flex md:justify-start md:px-6">
+        <div className="px-8 md:flex md:justify-center md:px-6">
             <main>
                 {children}
             </main>
--- frontend/app/api/ping/route.ts
+++ frontend/app/api/ping/route.ts
@@ -2,7 +2,6 @@ import { pingBackend } from "../../(main)/actions";
 
 
 export async function GET(req: Request) {
-    console.log('REQ', req)
     const pong = await pingBackend()
     return new Response(pong.message)
 }
